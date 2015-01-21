3da5ace60be23e6b096e801eeb23ba7aabc623f9
snorri-agust-snorrason <vaskur_@hotmail.com>
2015-01-21 22:39:12 +0000

    Kall og animation

--- Assets/Code/PlayerBehaviour.js
+++ Assets/Code/PlayerBehaviour.js
@@ -1,10 +1,47 @@
 ﻿#pragma strict
 
-	var speed : double;
-	var animator : Animator;
+var speed : float;
+var animator : Animator;
+
+var walk : float;
+var run : float;
+var playerIsWalking : boolean;
+var playerIsRunning : boolean;
 
 function Start () {
-	//animator = GetComponent<Animator>();
+	speed = 0;
+	walk = 60;
+	run = 90;
+	playerIsWalking = false;
+	playerIsWalking = false;
+	animator = GetComponent("Animator");
+}
+
+function WalkOrRun() {
+
+	if (Input.GetKey('w') || Input.GetKey('a') || Input.GetKey('s') || Input.GetKey('d')) {
+		playerIsWalking = true;
+	} else {
+		playerIsWalking = false; 
+	}
+	
+	 if (Input.GetKey(KeyCode.LeftShift)) {
+	 	playerIsRunning = true;
+	} else {
+	 	playerIsRunning = false;
+	 }	 
+}
+
+function SetSpeed() {
+	if (playerIsWalking) {
+		speed = walk;
+	}
+	if (playerIsRunning) {
+		speed = run;
+	}
+	if (!playerIsWalking && !playerIsRunning) {
+		speed = 0;
+	}
 }
 
 function FixedUpdate () {
@@ -29,12 +66,18 @@ function FixedUpdate () {
 	//Calculate the rotation in radians using trigometry
 	var AngleRad = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
 	//convert it to degrees, we subtract 180 because of it's original rotation
-    var AngleDeg = (180 / Mathf.PI) * AngleRad - 180;
+    var AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
     //set the rotation
     transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
 	
 	//Stop the guy from spinning
 	rigidbody2D.angularVelocity = 0;
+	
+	WalkOrRun();
+	SetSpeed();
+		
+	animator.SetFloat("Speed", Mathf.Abs(speed));
+		
 	
 	// Prints a joystick name if movement is detected.
 		// requires you to set up axes "Joy0X" - "Joy3X" and "Joy0Y" - "Joy3Y" in the Input Manger
