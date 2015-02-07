30d73a9dd96936f6d8a5df3227455a50bab9b7e5
Vilhelm Friðriksson <vfridriks@simnet.is>
2015-02-07 21:03:15 +0000

    Breyttur enemy behaviour code, settur upp odruvisi

--- Assets/Code/EnemyCode/EnemyBehaviour.js
+++ Assets/Code/EnemyCode/EnemyBehaviour.js
@@ -2,15 +2,15 @@
 //---------------------Variables--------------------------
 var mainPlayer : GameObject;
 
-var speed : float;
-
 // ---------||| Enemy Type Variables |||-----------
 var melee : boolean;
 var ranged: boolean;
 var ogre : boolean;
 var wolf : boolean;
+var health : double;
+var speed : float;
 
-//  ||| Combat system variables |||
+// ---------||| Combat system variables |||--------
 var meleeDist : double;
 var rangedWeapon : GameObject;
 var rangedTimer : float = 1;
@@ -18,9 +18,9 @@ var block : float;
 var attack : float;
 var fireBallCooldown : float = 0;
 
+// ---------||| Other variables |||--------
 var maxFollowDistance : double;
 var destroyable = 1;
-var health : double;
 var seePlayer;
 var isFollowingPath : boolean;
 var path : PathDefinition;
@@ -36,7 +36,13 @@ var animator : Animator;
 var isKneeling : boolean = false;
 
 var bloodPart : ParticleSystem;
+var blockParticles : ParticleSystem;
 //-------------------------------------------------------
+
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//------------------||| Setup & update functions |||---------------------
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function ogreSetup(){
 	health = 1.0;
@@ -48,7 +54,6 @@ function wolfSetup(){
 	speed = 60;
 }
 
-
 function Start () {
 	mainPlayer = GameObject.Find("Player");
 	
@@ -59,6 +64,7 @@ function Start () {
 	
 	animator =  GetComponent("Animator") as Animator;
 	bloodPart = transform.Find("BloodParticles").GetComponent(ParticleSystem);
+	blockParticles = transform.Find("BlockParticles").GetComponent(ParticleSystem);
 	
 	//Find the object for the path
 	var pathObject = FindClosestPath();
@@ -72,10 +78,11 @@ function Start () {
 	
 	if(ogre)ogreSetup();
 	if(wolf)wolfSetup();
+	else wolfSetup();
 }
 
 function FixedUpdate () {
-	//-----------------Timers and cooldowns----------------------
+	//-------------||| Timers and cooldowns |||------------------
 	if( isDead ){
 		rigidbody2D.velocity = Vector3(0,0,0);
 		deathTime -= Time.deltaTime;
@@ -102,6 +109,16 @@ function FixedUpdate () {
 }
 
 
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//------------------||| Path functions |||---------------------
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// returns Vector3 that is the position he should move towards if he is following path
+function getPathPoint()
+{
+	return path.getNextPosition(transform.position);
+}
+
 //returns the closest gameObject tagged as Path
 function FindClosestPath () : GameObject 
 {
@@ -123,31 +140,60 @@ function FindClosestPath () : GameObject
 		return closest;	
 }
 
+function followPath()
+{
+	var pathPoint = getPathPoint();
+	var vecToPath =  pathPoint - transform.position;
+			
+	var direction = vecToPath.normalized;
+	
+	
+	var AngleRad = Mathf.Atan2(vecToPath.y, vecToPath.x);
+	//convert it to degrees, we subtract 180 because of it's original rotation
+    var AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
+    //set the rotation
+    transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+	
+	//Stop the guy from spinning
+	rigidbody2D.angularVelocity = 0;
+	
+	direction.z=0;
+	rigidbody2D.AddForce(direction*speed);
+}
+
+function pushedBack(){
+	print("Blocked and push mafaka!");
+	rigidbody2D.AddForce(-transform.up * 250 );
+	mainPlayer.rigidbody2D.AddForce(-mainPlayer.transform.up * 150);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//------------------||| Combat functions |||---------------------
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// When hit by kneel before the king the enemy
+// starts bleeding and moves slower.
 function kneel(kneelSpeed : float ){
 	isKneeling = true;
 	bloodPart.Play();
 	speed = kneelSpeed;
 }
 
-function OnTriggerEnter2D (other : Collider2D) {
-		if(other.gameObject.tag == "Player"){
-			var player : PlayerBehaviour = mainPlayer.GetComponent(PlayerBehaviour);
-			player.collisionWithEnemy(gameObject);
-		}
-}
-
+// Every time the enemy is hit there is a 1/5 chance of it
+// being able to block.
 function tryToBlock(){
-	var chanceOfBlocking = Random.Range(0, 3);
-	print(chanceOfBlocking);
+	var chanceOfBlocking = Random.Range(0, 5);
 	if(chanceOfBlocking < 1) {
 		transform.Find("Bubble").GetComponent(SpriteRenderer).enabled = true;
+		blockParticles.Play();
 		block = 1;
 	}
 }
-	
+
+// Instant death when enemy does not see player.
+// Melee enemies try to block.
 function gotHit(){
-	if(!seePlayer){
-		rigidbody2D.AddForce(transform.up*5000);	
+	if(!seePlayer){	
 		bleed();
 		isDead = true;
 		disapleColliders();
@@ -170,6 +216,87 @@ function gotHit(){
 	}
 }
 
+function attackMelee(vecToPlayer : Vector3){
+	var direction = vecToPlayer.normalized;
+	if(vecToPlayer.magnitude > meleeDist){
+		rigidbody2D.AddForce(direction*speed);	
+	}
+	if(block > 0){
+	 	block -= Time.deltaTime;
+		return;
+	}
+	else if(attack > 0){
+		attack -= Time.deltaTime;
+		return;
+	}
+	else{
+		var decision = Random.Range(0.0,2.0);
+		print(decision);
+		if(decision <= 1){
+			//TODO Attack function
+			rigidbody2D.AddForce(direction*200);
+			attack = 1.5;
+			animator.SetBool("isAttacking", true);
+			return;
+		}
+	
+	
+		return;
+	}
+}
+
+function attackRanged(vecToPlayer : Vector3){
+	if(fireBallCooldown <= 0){
+		var fireball = Instantiate (rangedWeapon, transform.position, transform.rotation);
+		fireBallCooldown = 2;
+		animator.SetBool("isAttacking", true);
+	}
+	else{
+	var shouldMove = Random.Range(0, 3);
+	if(shouldMove == 0) rigidbody2D.AddForce(transform.up*speed);
+	if(shouldMove == 1) rigidbody2D.AddForce(-transform.up*speed);	
+	}
+	//var FireballShot = Instantiate(Fireball, transform.position, transform.rotation);
+	return;
+}
+
+function attackPlayer()
+{
+	var vecToPlayer = Vector3(0,0,0);
+	if(mainPlayer!=null)
+		vecToPlayer =  mainPlayer.transform.position - transform.position;
+
+	if(vecToPlayer.magnitude > maxFollowDistance)
+		return;
+			
+	var direction = vecToPlayer.normalized;
+	//Get player position
+	var playerPos = mainPlayer.transform.position;
+	//Calculate the rotation in radians using trigometry
+	var AngleRad = Mathf.Atan2(playerPos.y - transform.position.y, playerPos.x - transform.position.x);
+	//convert it to degrees, we subtract 180 because of it's original rotation
+    var AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
+    //set the rotation
+    transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+	
+	//Stop the guy from spinning
+	rigidbody2D.angularVelocity = 0;	
+	
+	if(ranged)attackRanged(vecToPlayer);
+	if(melee) attackMelee(vecToPlayer);
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//------------------||| Collision functions |||---------------------
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function OnTriggerEnter2D (other : Collider2D) {
+		if(other.gameObject.tag == "Player"){
+			var player : PlayerBehaviour = mainPlayer.GetComponent(PlayerBehaviour);
+			player.collisionWithEnemy(gameObject);
+		}
+}
+
 function disapleColliders(){
 	var coll : BoxCollider2D[];
 	coll = gameObject.GetComponents.<BoxCollider2D>();
@@ -178,14 +305,13 @@ function disapleColliders(){
 	}
 }
 
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//------------------||| Other functions |||---------------------
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function bleed(){
 	bloodPart.Play();
-}
-
-// returns Vector3 that is the position he should move towards if he is following path
-function getPathPoint()
-{
-	return path.getNextPosition(transform.position);
 }
 
 function canISeePlayer()
@@ -217,99 +343,5 @@ function canISeePlayer()
 }
 
 
-function attackMelee(vecToPlayer : Vector3){
-	var direction = vecToPlayer.normalized;
-	if(vecToPlayer.magnitude > meleeDist){
-		rigidbody2D.AddForce(direction*speed);	
-	}
-	if(block > 0){
-	 	block -= Time.deltaTime;
-		return;
-	}
-	else if(attack > 0){
-		attack -= Time.deltaTime;
-		return;
-	}
-	else{
-		var decision = Random.Range(0.0,2.0);
-		print(decision);
-		if(decision <= 1){
-			//TODO Attack function
-			//rigidbody2D.AddForce(direction*1500);
-			attack = 3;
-			animator.SetBool("isAttacking", true);
-			return;
-		}
-	
-	
-		return;
-	}
-}
-
-function attackRanged(vecToPlayer : Vector3){
-	if(fireBallCooldown <= 0){
-		var fireball = Instantiate (rangedWeapon, transform.position, transform.rotation);
-		fireBallCooldown = 2;
-	}
-	else{
-	var shouldMove = Random.Range(0, 3);
-	if(shouldMove == 0) rigidbody2D.AddForce(transform.up*speed);
-	if(shouldMove == 1) rigidbody2D.AddForce(-transform.up*speed);	
-	}
-	//var FireballShot = Instantiate(Fireball, transform.position, transform.rotation);
-	return;
-}
 
 
-function attackPlayer()
-{
-	var vecToPlayer = Vector3(0,0,0);
-	if(mainPlayer!=null)
-		vecToPlayer =  mainPlayer.transform.position - transform.position;
-
-	if(vecToPlayer.magnitude > maxFollowDistance)
-		return;
-			
-	var direction = vecToPlayer.normalized;
-	//Get player position
-	var playerPos = mainPlayer.transform.position;
-	//Calculate the rotation in radians using trigometry
-	var AngleRad = Mathf.Atan2(playerPos.y - transform.position.y, playerPos.x - transform.position.x);
-	//convert it to degrees, we subtract 180 because of it's original rotation
-    var AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
-    //set the rotation
-    transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
-	
-	//Stop the guy from spinning
-	rigidbody2D.angularVelocity = 0;	
-	
-	if(ranged)attackRanged(vecToPlayer);
-	if(melee) attackMelee(vecToPlayer);
-}
-
-function followPath()
-{
-	var pathPoint = getPathPoint();
-	var vecToPath =  pathPoint - transform.position;
-			
-	var direction = vecToPath.normalized;
-	
-	
-	var AngleRad = Mathf.Atan2(vecToPath.y, vecToPath.x);
-	//convert it to degrees, we subtract 180 because of it's original rotation
-    var AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
-    //set the rotation
-    transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
-	
-	//Stop the guy from spinning
-	rigidbody2D.angularVelocity = 0;
-	
-	direction.z=0;
-	rigidbody2D.AddForce(direction*speed);
-}
-
-function pushedBack(){
-	print("Blocked and push mafaka!");
-	rigidbody2D.AddForce(-transform.up * 1000 );
-	mainPlayer.rigidbody2D.AddForce(-mainPlayer.transform.up * 250);
-}
