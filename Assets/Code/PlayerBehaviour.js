1b22e0e61efaec213f8d511ee6032f34923a8a90
unknown <ahh19@hi.is>
2015-02-13 21:52:36 +0000

    strange deaths

--- Assets/Code/PlayerBehaviour.js
+++ Assets/Code/PlayerBehaviour.js
@@ -36,7 +36,7 @@ function Start () {
 	attackCooldown = 0;
 	isAlive = true;
 	crownIsOn = true;
-	health = 3;
+	//health = 3;
 	animator =  GetComponent("Animator") as Animator;
 	fetchFromMaster();
 	initializeParticleSystems();
